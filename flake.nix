65efdcdf53efa328603783e012b4f4c9fa05f7e9
Graham Christensen <grahamc@Grahams-MacBook-Air.local>
2023-08-22 09:11:15 -0400

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "My Nix project";
+
+  inputs = {
+    nixpkgs.url = "https://api.flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [ terraform go_1_20 ];
+      };
+    };
+}
